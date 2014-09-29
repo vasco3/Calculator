fa06c9c4460a93fcda42fd2b1ec3f8ae77721259
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2014-09-29 05:30:53 +0000

    added constants

---
+++ app/scripts/constants/Constants.js
@@ -0,0 +1,10 @@
+var keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({
+	CALCULATOR_ADD: null,
+	CALCULATOR_SUBTRACT: null,
+	CALCULATOR_MULTIPLY: null,
+	CALCULATOR_DIVIDE: null,
+	CALCULATOR_UPDATE_INPUT: null
+});
+
