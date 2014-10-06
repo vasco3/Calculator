ffe32ba660200cbe63d49e9794be70947c96617f
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2014-10-06 03:16:50 +0000

    Added onChange and Readonly

--- app/scripts/components/number-input.js
+++ app/scripts/components/number-input.js
@@ -5,12 +5,33 @@
 var React = require('react');
 
 var NumberInput = React.createClass({
+	propTypes: {
+		value: React.PropTypes.string
+	},
+
+	getInitialState: function() {
+		return {
+			value: this.props.value || '0' 
+		};
+	},
 
 	render: function() {
 		return (
-		    <input className="display js-js-display" 
-		    	placeholder="0" type="text" />
+		    <input className="display" 
+		    	readOnly
+		    	value={this.props.value}
+		    	type="text" />
 		);
+	},
+
+	/**
+	 * on input Change
+	 * @param  {object} event
+	 */
+	_onChange: function (event) {
+		this.setState({
+			value: event.target.value
+		});
 	}
 	
 });
