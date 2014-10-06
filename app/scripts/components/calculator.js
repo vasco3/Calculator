7eea8d0e0ae2e8bc166aa9ec99ce2c49ca1a0b76
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2014-10-06 03:15:45 +0000

    passed onClicks to operation buttons

--- app/scripts/components/calculator.js
+++ app/scripts/components/calculator.js
@@ -9,33 +9,55 @@ var React = require('react');
 var NumberInput = require('./number-input');
 var ButtonDigit = require('./button-digit');
 var ButtonOperation = require('./button-operation');
+var Store = require('../stores/Store');
+var Actions = require('../actions/Actions');
 
 var Calculator = React.createClass({
+
+	getInitialState: function() {
+		return {
+			input: Store.get()
+		};
+	},
+
+	componentDidMount: function() {
+		Store.addChangeListener(this._onChange);
+	},
+
+	componentWillMount: function() {
+		Store.removeChangeListener(this._onChange);
+	},
 
 	render: function() {
 		var equalSymbol = '=';
 		
 		return (
 			<div className="container calculator clearfix">
-				<NumberInput defaultValue="0" />
+				<NumberInput value={this.state.input} />
 				<ButtonDigit text="1" />
 				<ButtonDigit text="2" />
 				<ButtonDigit text="3" />
-				<ButtonOperation text="/" />
-				<ButtonOperation text="x" />
+				<ButtonOperation text="/" onClick={Actions.divide} />
+				<ButtonOperation text="x" onClick={Actions.multiply} />
 				<ButtonDigit text="4" />
 				<ButtonDigit text="5" />
 				<ButtonDigit text="6" />
 				<ButtonDigit text="7" />
 				<ButtonDigit text="8" />
 				<ButtonDigit text="9" />
-				<ButtonOperation text="+" />
-				<ButtonOperation text="-" />
+				<ButtonOperation text="+" onClick={Actions.add} />
+				<ButtonOperation text="-" onClick={Actions.subtract}  />
 				<ButtonDigit text="." />
 				<ButtonDigit text="0" />
-				<ButtonOperation text="="/>
+				<ButtonOperation text="=" onClick={Actions.divide} />
 			</div>
 		);
+	},
+
+	_onChange: function () {
+		this.setState({
+			input: Store.get() 
+		});
 	}
 });
 
